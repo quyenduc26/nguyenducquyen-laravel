37bfba84a3c5b94465d9ca30760de29e3306e14b
unknown <quyenduc26@gmail.com>
2024-02-27 23:31:37 +0700

    done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,45 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
-    //Action index
+    public $data = [];
     public function index(){
-        
-    $title = "Học lập trình web tại Unicode";
-    $conntent = "Học lập trình Laravel 8.x tại Unicode";
-    
-    /*
-    [
-        'title' =>$title,
-        'conntent' => $conntent
-    ]
-        compact('title', 'conntent')
-    */
 
+        $this->data['welcome'] = 'Học lập trình laravel tại <b>Unicode</b>';
+        $this->data['content'] = '<h3>Chương 1: Nhập môn Laravel</h3>
+        <p>Kiến thức 1</p>
+        <p>Kiến thức 2</p>
+        <p>Kiến thức 3</p>';
 
+        $this->data['index'] =1;
 
+        $this->data['dataArr'] = [];
 
-    return view('home' )->with(['title'=> $title, 'conntent' =>$conntent]);//load view hơe.php
-   
+        $this->data['number'] = 3;
 
-   // return View::make('home')->with(['title'=> $title, 'conntent' =>$conntent]) ;
-
-   //$conntentView = view('home')->render();
-   //$conntentView = $conntentView->render();
-   //dd($conntentView);
-   //echo $conntentView;
-}
-    //Action getNews
-    public function getNews(){
-        return 'Danh sách tin tức';
-    }
-    public function getCategories($id){
-        return 'Chuyên mục: '.$id;
-    }
-    public function getProductDetail($id){
-        return view('clients.products.detail', compact('id'));
+        return view('home', $this->data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Unicode-Học lập trình web cơ bản</title>
-</head>
-<body>
-    <header>
-        <h1>HEADER - UNICODE</h1>
-        <h2>
-          {{$title}}
-        </h2>
-    </header>
-    <main>
-        <h1>NỘI DUNG - UNICODE </h1>
-        <h2>
-            <?php  echo $conntent ?>
-        </h2>
-    </main>
-    <header>
-        <h1>FOOTER - UNICODE </h1>
-    </header>
-</body>
-</html>
+<h1>Trang chủ Unicode</h1>
+<h2> {{request()->keyword?request()->keyword:'Không có gì';}}</h2>
+<div class="container">
+    {!! !empty($content)?$content:false !!}
+</div>
+<hr>
+
+@for ($i  = 1; $i<=10; $i++)
+    @if ($i==5)
+        @continue
+    @endif
+    <p>Phần tử thứ: {{$i}}</p>
+
+@endfor
